634290e132599fa40b9da4e59b47e17f766907a5
Morgana Bagatini <morganabagatini@gmail.com>
2021-01-15 07:02:36 -0300

    Criação do JWT

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+	jwt: {
+		secret: '02e4de3963fc80349ba42215b05483ca',
+		expiresIn: '1d'
+	}
+}
---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+
+// video 12:40
+
+export default function ensureAuthenticated(request: Request, response: Response, next: NextFunction): void {
+	const authHeader = request.headers.authorization;
+
+	if (!authHeader) {
+		throw new Error("JWT token is missing");
+	}
+
+	// separar o Barear do token
+	const [, token] = authHeader.split(' ');
+
+	try {
+		const decoded = verify(token, authConfig.jwt.secret);
+		return next();
+	} catch {
+		throw new Error("Invalid JWT token");
+	}
+}
