74a2429624485c59a624717335085be04afc8c59
Morgana Bagatini <morganabagatini@gmail.com>
2021-02-06 18:28:22 -0300

    CORS

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import AppError from './errors/AppError';
 import routes from './routes';
@@ -10,6 +11,9 @@ import uploadConfig from './config/upload';
 import './database';
 
 const app = express();
+
+// Controla quem pode acessar a API através do browser
+app.use(cors());
 
 app.use(express.json());
 
